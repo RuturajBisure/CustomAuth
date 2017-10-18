7cfb4ea35a720c7fe5afd6b6f1df797d01016014
Sandrosss <ruturaj@ascratech.in>
2017-10-18 11:54:07 +0530

    mailer for forgot password done on Be

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,10 @@
 class UserMailer < ActionMailer::Base
+  default :from => "chandlersparrow3@gmail.com"
 
   def welcome_email(user)
       @user = user
       @url = "http://localhost:3000/sign_in"
-      @site_name = "localhost"
+      @site_name = "localhost:3000"
       mail(:to => user.email, :subject => "Welcome to my website.")
   end
   def reset_password_email(user)
