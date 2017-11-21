207dcb268f9d344cfc75f7f28b442f6c3623c05c
Sandrosss <ruturaj@ascratech.in>
2017-11-21 17:36:33 +0530

    extra changes removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,15 +38,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.gmail.com',
-  #   port:                 587,
-  #   domain:               'gmail.com',
-  #   user_name:            'ruturaj@ascratech.in',
-  #   password:             'sandrosss...',
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true  
-  # }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
